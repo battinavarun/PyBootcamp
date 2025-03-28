664e871aafa4d044f0b6adeea3d980924070751d
Varun 31003 <battinavarun@gmail.com>
2025-03-28 12:03:54 +0530

    Initial commit

---
+++ Loops_Demo.py
@@ -0,0 +1,12 @@
+# names = ["Buddi","Varun","Shinchan"]
+# for name in names:
+#     print(name)
+
+student_scores = [12,34,64,34,76,43,23,22]
+total = sum(student_scores)
+# print(total)
+
+sum = 0;
+for i  in student_scores:
+    sum += i
+print(sum)
